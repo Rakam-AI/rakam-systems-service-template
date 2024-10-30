c6f66eba4366b474f68624371375aa6478430850
pengZHENG-e <curtis.zp@outlook.com>
2024-10-30 17:26:38 +0000

    ft system manager

--- application/engine/components.py
+++ application/engine/components.py
@@ -1,12 +1,17 @@
+from rakam_systems.system_manager import SystemManager
+
 from rakam_systems.components.data_processing.data_processor import DataProcessor
 from rakam_systems.components.vector_search.vector_store import VectorStore
 from rakam_systems.components.vector_search.vs_manager import VSManager
 from rakam_systems.components.generation.generation import Generator
 from rakam_systems.components.rag_connectors.generation_feeder import GenerationFeeder
 
+base_index_path="data/vector_stores_for_test/example_baseIDXpath"
+system_manager = SystemManager(system_config='system_config.yaml')
+
 data_processor = DataProcessor()
-VS = VectorStore(base_index_path="data/vector_stores_for_test/example_baseIDXpath", embedding_model="sentence-transformers/all-MiniLM-L6-v2")
-vsManager = VSManager(vector_store=VS,data_processor=data_processor)
+vsManager = VSManager(base_index_path=base_index_path,embedding_model= "sentence-transformers/all-MiniLM-L6-v2", system_manager=system_manager)
+VS = VectorStore(base_index_path=base_index_path, embedding_model="sentence-transformers/all-MiniLM-L6-v2")
 generator = Generator(model="gpt-4o-mini")
 ragGen = GenerationFeeder(generator_model=generator, vs_manager=vsManager)
 
